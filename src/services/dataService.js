922328806721b0debccf217c645cc78dd034647b
tom-sapletta-com <tom@sapletta.com>
2025-07-01 10:40:49 +0200

    update

--- src/services/dataService.js
+++ src/services/dataService.js
@@ -6,7 +6,7 @@ let benefitConfig = {};
 const countryDataCache = new Map();
 
 // Supported country codes
-const SUPPORTED_COUNTRIES = ['DE', 'FR', 'ES', 'IT', 'NL'];
+const SUPPORTED_COUNTRIES = ['DE', 'FR', 'ES', 'IT', 'NL', 'PT'];
 
 // Constants
 const DAYS_IN_YEAR = 365;
